e413a1153843f5b8b8904c4ba5bcd50a769e1f24
Speedkicks <spysrs2@gmail.com>
2015-07-15 18:15:42 -0400

    Added GameState manager for omniscience

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,15 +1,30 @@
 
-var _boardSize = 8;
-var _squareWidth = 75;
+var GameManager = {
+    //Enums
+    EMTPY: 0,
+    RED: 1,
+    BLACK: 2,
+    //Constants
+    boardSize: 8,
+    squareWidth: 75,
+
+    //State array
+    gamestate: []
+}
 
 var canvas = document.getElementById('game');
 var ctx = canvas.getContext('2d');
 
 
-//Draw board
-for(var i = 0; i < _boardSize; i++){
 
-    for(var j = 0; j < _boardSize; j++){
+//Draw board and fill gamestate array
+for(var i = 0; i < GameManager.boardSize; i++){
+
+    GameManager.gamestate[i] = new Array();
+
+    for(var j = 0; j < GameManager.boardSize; j++){
+
+        GameManager.gamestate[i][j] = GameManager.EMPTY;
 
         //Alternate square color
         if( (i+j)%2 == 1 )                  
@@ -18,42 +33,46 @@ for(var i = 0; i < _boardSize; i++){
             ctx.fillStyle = "rgb(255, 255, 255)"; 
 
         //Draw square
-        ctx.fillRect( j * _squareWidth, i * _squareWidth, _squareWidth, _squareWidth);
+        ctx.fillRect( j * GameManager.squareWidth, i * GameManager.squareWidth, GameManager.squareWidth, GameManager.squareWidth);
+
         
     }
 }
 
 //Create red pieces
 var red_pieces = [];
-for(var i = 0; i < _boardSize; i++){
+for(var i = 0; i < GameManager.boardSize; i++){
 
     for(var j = 0; j < 3; j++){
 
-        if( (i+j)% 2 == 0 )
-            red_pieces.push(new GAME_PIECE(i, j, 'RED'));
+        if( (i+j)% 2 == 0 ){
+            GameManager.gamestate[i][j] = GameManager.RED;
+            red_pieces.push(new GAME_PIECE(i, j, GameManager.RED));
+        }
 
     }
 }
 
 //Create black pieces
 var black_pieces = [];
-for(var i = 0; i < _boardSize; i++){
+for(var i = 0; i < GameManager.boardSize; i++){
 
-    for(var j = _boardSize - 1; j >= _boardSize - 3; j--){
-        if( (i+j) % 2 == 0 )
-            black_pieces.push(new GAME_PIECE(i, j, 'BLACK'));
+    for(var j = GameManager.boardSize - 1; j >= GameManager.boardSize - 3; j--){
+
+        if( (i+j) % 2 == 0 ){
+            GameManager.gamestate[i][j] = GameManager.BLACK; 
+            black_pieces.push(new GAME_PIECE(i, j, GameManager.BLACK));
+        }
+
     }
 }
 
 
 //Draw red pieces
-ctx.fillStyle = 'rgb(200, 0, 0)';
-red_pieces.forEach( function(element, index, array){
-    element.draw(_squareWidth, ctx);
+red_pieces.forEach(function(element, index, array){
+    element.draw(GameManager.squareWidth, ctx);
 });
 
-//Draw black pieces
-ctx.fillStyle = 'rgb(100, 100, 100)';
-black_pieces.forEach( function(element, index, array){
-    element.draw(_squareWidth, ctx);
-})
+black_pieces.forEach(function(element, index, array){
+    element.draw(GameManager.squareWidth, ctx);
+});
--- src/scripts/piece.js
+++ src/scripts/piece.js
@@ -4,7 +4,15 @@ var GAME_PIECE = function(initX, initY, color){
 
     this.x = initX;
     this.y = initY;
-    this.color = color;
+    this.state_color = color;
+
+    //Hardcode the enumeration because Javascript
+    if(color == 1)
+        this.fill_color = 'rgb(200, 0, 0)';
+    else if(color == 2)
+        this.fill_color = 'rgb(100, 100, 100)';
+
+    console.log('Color: ' + color + '\nFill Color: ' + this.fill_color);
 
     this.draw = function(squareWidth, ctx){
         var x_position = (this.x * squareWidth) + squareWidth/2;
@@ -15,8 +23,8 @@ var GAME_PIECE = function(initX, initY, color){
         var path = new Path2D();
         path.arc(x_position, y_position, radius, 0, Math.PI*2, false);
 
-        console.log('x: ' + x_position + '\ny:' + y_position);
 
+        ctx.fillStyle = this.fill_color;
         ctx.fill(path);
         ctx.stroke(path);
     }
